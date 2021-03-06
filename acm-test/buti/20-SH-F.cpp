3c17bf802582c72198dd6ad49c5355d5f3bf6759
KomeijiGreen <746530916@qq.com>
2021-03-07 03:05:16 +0800

    lunkuoxian

--- acm-test/buti/20-SH-F.cpp
+++ acm-test/buti/20-SH-F.cpp
@@ -19,10 +19,10 @@ void change(int *a, int x) {
 }
 
 int ichange(int *a) {
-    int res = 0, r = 0, c = 0, j = 0;
+    int res = 0, r = 0, c = 0, j = -1;
     a[0] = 0;
     for(int i = 1; i <= n; ++i) {
-        j += (a[i] - a[i-1]);
+        j += (a[i] - a[i-1] + 1);
         res |= (1<<j);
     }
     return res;
@@ -36,6 +36,13 @@ struct I {
         return w > b.w;
     }
 } uu[100];
+
+void chk(ll &x, ll y) {
+    if(x < y) x = y;
+}
+
+int pos[50];
+ll ans[50];
 
 int main() {
     cin >> n; m = 2 * n;
@@ -59,20 +66,65 @@ int main() {
     }
     int st = 0;
     int num = n * (n + 1) / 2;
-    for(int j = 0; j <= num; ++j) {
-        for(int k = 0; k < sz; ++k) dp[st][j][k] = -1;
+    sort(uu + 1, uu + num + 1);
+    for(int i = 0; i < 2; ++i) {
+        for(int j = 0; j <= num; ++j) {
+            for(int k = 0; k < sz; ++k) dp[i][j][k] = -1;
+        }
     }
     int o = 0;
     for(int j = 0; j < n; ++j) o |= (1<<j);
     dp[st][0][o] = 0;
-    int cur, nex;
+    int cur, nex, x, y, cnt;
+    ll w, cost;
     for(int i = 0; i < num; ++i, st ^= 1) {
+        x = uu[i+1].x; y = uu[i+1].y; w = uu[i+1].w;
         for(int j = 0; j <= i; ++j) {
             for(int k = 1; k <= tot; ++k) {
                 cur = li[k];
-                if(dp[st][j][cur] == -1) continue;
+                cost = dp[st][j][cur];
+                //chk(ans[j], cost);
+
+                if(cost == -1) continue;
+                chk(dp[st^1][j][cur], cost);
+
+                cnt = 0;
+                change(pos, cur);
+                for(int r = y; r <= n; ++r) {
+                    if(pos[r] < x) {
+                        cnt += x - pos[r];
+                        pos[r] = x;
+                    }
+                }
+
+                //printf("cnt: %d\n", cnt);
+                //printf("wei: %lld\n", w);
+                nex = ichange(pos);
+                chk(dp[st^1][j+1][nex], cost + w * cnt);
 
             }
         }
+        for(int j = 0; j <= i; ++j) {
+            for(int k = 1; k <= tot; ++k) {
+                cur = li[k];
+                dp[st][j][cur] = -1;
+            }
+        }
+    }
+    for(int j = 0; j <= num; ++j) {
+        for(int k = 1; k <= tot; ++k) {
+            cur = li[k];
+            chk(ans[j], dp[st][j][cur]);
+        }
+    }
+
+    ll sum = 0;
+    for(int i = 1; i <= num; ++i) sum += uu[i].w;
+
+    //printf("sum: %lld\n", sum);
+    //printf("tot: %d\n", tot);
+
+    for(int i = 1; i <= num; ++i) {
+        printf("%lld\n", sum - ans[i]);
     }
 }
